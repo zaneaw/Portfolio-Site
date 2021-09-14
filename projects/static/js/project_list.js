673262d13b974e13c6aabe8743a97328eb3621a1
Zane Wilson <wilsonzanea@gmail.com>
2021-09-14 15:34:49 -0400

    more testing with resume viewer

---
+++ projects/static/js/project_list.js
@@ -0,0 +1,45 @@
+
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "LIGHT_BOX"
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("resume_button").disabled = false;
+});
+
+/* Function to render the file using PDF Embed API. */
+function previewFile()
+{
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "52d6bfb1b3564201a2ef68f8715a4f80"
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "https://documentcloud.adobe.com/link/track/?locale=en-US&uri=urn%3Aaaid%3Ascds%3AUS%3ApXoGvgcURX-FCJ4GB4uPhw",
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "Resume.pdf"
+        }
+    }, viewerConfig);
+};
