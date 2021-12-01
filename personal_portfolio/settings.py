97dbec9b399cead8265bb18cd1e0d164ccacb7ff
Zane Wilson <wilsonzanea@gmail.com>
2021-12-01 17:02:10 -0500

    .

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ APP_NAME = "Zane's Portfolio Website"
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
