87f5772f01c1a229fce31b9de5f6484af6b34b72
Zane Wilson <wilsonzanea@gmail.com>
2021-12-01 17:47:20 -0500

    fix secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ APP_NAME = "Zane's Portfolio Website"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
