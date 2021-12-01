dafd59f2d73813c02b751d251684fb4992c31ba7
Zane Wilson <wilsonzanea@gmail.com>
2021-12-01 01:12:15 -0500

    add file_size checker. upload size <= 2.5MB

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.utils.encoding import force_text
 from ckeditor.fields import RichTextField
 
 
+def file_size(value):
+    limit = 2621440 # 2.5 * 1024 * 1024, or 2.5MB
+    if value.size > limit:
+        raise ValidationError('File size is too large. Size should not exceed 2.5MB')
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     about = RichTextField(blank=True, null=True, max_length=1000) 
-    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile")
+    profile_pic = models.ImageField(null=True, blank=True, validators=[file_size], upload_to="images/profile")
     facebook_url = models.CharField(max_length=255, null=True, blank=True)
     twitter_url = models.CharField(max_length=255, null=True, blank=True)
     instagram_url = models.CharField(max_length=255, null=True, blank=True)
--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.conf import settings
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 
 
+def file_size(value):
+    limit = 2621440 # 2.5 * 1024 * 1024, or 2.5MB
+    if value.size > limit:
+        raise ValidationError('File size is too large. Size should not exceed 2.5MB')
+
+
 class Project(models.Model):
     title = models.CharField(max_length=30, validators=[MinLengthValidator(2, "Must be more than 2 characters."), MaxLengthValidator(30, "Must be less than 30 characters.")])
     desc = RichTextField(blank=True, null=True)
     repo = models.URLField(blank=True)
-    image = models.ImageField(null=True, blank=True, upload_to="images/projects")    
+    image = models.ImageField(null=True, blank=True, validators=[file_size], upload_to="images/projects")
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='liked_projects')
 
