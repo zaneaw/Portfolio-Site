07037e22bc3e05ce93efe399a09c09fc38ffc3ff
Zane Wilson <wilsonzanea@gmail.com>
2022-12-13 22:21:30 -0500

    new allowed host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'zane-wilson.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'zane-wilson.com', '.railway.app']
 
 
 # Application definition
